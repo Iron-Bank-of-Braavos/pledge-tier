5270a2b71e330937579585c12898e19c8fd79e49
milkncookiesss <p.parkcontact@gmail.com>
2018-09-06 13:25:28 -0700

    "created functions to generate random pledge ammounts and backers"

--- database/filldb.js
+++ database/filldb.js
@@ -1,6 +1,20 @@
+const model = require('./models.js');
 //fill db with documents
 
 //create a random num gen for Pledge $
+const createByFive = () => {
+  let num = Math.floor(Math.random() * 1000);
+  while (num % 5 !== 0) {
+    num--;
+  };
+  return num;
+};
+
+const createBackers = () => {
+  let backers = Math.floor(Math.random() * 300);
+  return backers;
+};
+
 
 //hit random gen api to get reward(s)
 //hit random date api to get ETA
--- database/models.js
+++ database/models.js
@@ -1,7 +1,17 @@
+const db = require('./index.js');
 
+const pledgeSchema = mongoose.Schema({
+  pledge_ammount: {type: Number, required: true},
+  reward: {type: String, required: true},
+  eta: {type: Date, required: true},
+  shipping_location: {type: String, required: true},
+  backers: {type: Number, required: true}
+});
 
+const Pledge = mongoose.model('Pledge', pledgeSchema);
 //Pledge #
 //Reward(s)
 //ETA date
 //shipping location
 //Backere #
+
