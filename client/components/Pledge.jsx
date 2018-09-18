5944b4fc9a6568cc56636431ba49656413498135
milkncookiesss <p.parkcontact@gmail.com>
2018-09-18 13:56:47 -0700

    "updating pledge module"

--- client/components/Pledge.jsx
+++ client/components/Pledge.jsx
@@ -38,6 +38,7 @@ class Pledge extends React.Component {
     this.state = {
       showButton: false
     }
+    this.handleHover = this.handleHover.bind(this);
   }
 
   handleHover() {
@@ -47,26 +48,31 @@ class Pledge extends React.Component {
   }
 
   render() {
-
+    // this.state.showButton ? console.log('yas') : console.log('nah');
     return(
       //on hover show button
-      <ul className="Pledge">
-        <div className={styles.container}>
-          <span>Pledge ${props.pledge.pledge_ammount} or more</span>
-          <h3>
-            Control Base Game
-          </h3>
-          <span>{props.pledge.reward}</span>
-          <div>
-            <span>ESTIMATED DELIVERY</span>
-            <span>{props.pledge.eta}</span>
-          </div>
-          <div>
-            <span>SHIPS TO</span>
-            <span>{props.pledge.shipping_location}</span>
-          </div>
-          <div>
-            <span>{props.pledge.backers} backers</span>
+      <ul>
+        {/* <div class="text">select this pledge</div> */}
+        <div  className={styles.container}>
+          <div className={styles.hover} onMouseEnter={this.handleHover} onMouseLeave={this.handleHover}>
+            <span>Pledge ${this.props.pledge.pledge_ammount} or more</span>
+            <div className={styles.text}>
+              <h3>
+                Rewards
+              </h3>
+            </div>
+            <ul className={styles.text}>{this.props.pledge.reward}</ul>
+            <div>
+              <span>ESTIMATED DELIVERY</span>
+              <span>{this.props.pledge.eta}</span>
+            </div>
+            <div>
+              <span>SHIPS TO</span>
+              <span>{this.props.pledge.shipping_location}</span>
+            </div>
+            <div>
+              <span>{this.props.pledge.backers} backers</span>
+            </div>
           </div>
         </div>
       </ul>
--- client/components/Support.jsx
+++ client/components/Support.jsx
@@ -5,16 +5,32 @@ class Support extends React.Component {
     super(props);
 
     this.state = {
-
+      pledge: ''
     }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log('the pledge ', this.state.pledge);
+    this.setState({
+      pledge: ''
+    });
+  }
+
+  handleChange(e) {
+    this.setState({
+      pledge: e.target.value
+    });
   }
 
   render() {
     return (
       <div>
       <h3>Support</h3>
-      <form>
-        <input />
+      <form onSubmit={this.handleSubmit}>
+        <input onChange={this.handleChange} value="5"/>
       </form>
     </div>
     )
