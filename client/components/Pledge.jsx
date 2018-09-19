82a5f2603f78ca2adf5d6629ec7d1a39913ef099
milkncookiesss <p.parkcontact@gmail.com>
2018-09-19 10:55:11 -0700

    "hover over button works"

--- client/components/Pledge.jsx
+++ client/components/Pledge.jsx
@@ -1,37 +1,5 @@
 import styles from './CSS/Pledge.css';
 import React from 'react';
-// import reward from './Pledge Image/select_reward.png'
-
-// const Container = {
-//   border: '10px', 
-//   color: '#000000',
-//   // border-radius: '3px',
-// }
-
-// const Pledge = (props) => {
-// 	return(
-// 		<ul className="Pledge">
-// 			<div className={styles.container}>
-// 				<span>Pledge ${props.pledge.pledge_ammount} or more</span>
-// 				<h3>
-//           Control Base Game
-// 				</h3>
-// 				<span>{props.pledge.reward}</span>
-//         <div>
-//           <span>ESTIMATED DELIVERY</span>
-// 				  <span>{props.pledge.eta}</span>
-//         </div>
-//         <div>
-//           <span>SHIPS TO</span>
-// 				  <span>{props.pledge.shipping_location}</span>
-//         </div>
-//         <div>
-// 				  <span>{props.pledge.backers} backers</span>
-//         </div>
-// 			</div>
-// 		</ul>
-// 	);
-// };
 
 class Pledge extends React.Component {
   constructor(props) {
@@ -53,14 +21,15 @@ class Pledge extends React.Component {
     // this.state.showButton ? console.log('yas') : console.log('nah');
     return(
       //on hover show button
-      <ul>
-        {/* <div class="text">select this pledge</div> */}
+      <div > 
+        <ul>
         <div className={styles.container} onMouseOver={this.handleHover}>
           <div className={styles.overlay}>
-            <div className={styles.text}>Select this Reward</div>
+            <div className={styles.select}>Select this Reward</div>
           </div>
           <div>
-            <span>Pledge ${this.props.pledge.pledge_ammount} or more</span>
+            <h2 className={styles.pledge_ammount}>Pledge</h2>
+            <span className={styles.money}> ${this.props.pledge.pledge_ammount} or more</span>
             <div>
               <h3>
                 Rewards
@@ -81,6 +50,8 @@ class Pledge extends React.Component {
           </div>
         </div>
       </ul>
+      </div>
+      
     );
   }
 }
